fde977776d8cf57534eddf2bae852ee0c1e34d43
Bruno Alves <vespidhook@gmail.com>
2023-07-03 23:41:49 -0300

    first commit

---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,42 @@
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
+import { HabitDay } from "./HabitDay";
+
+const weekDays = [
+  'D',
+  'S',
+  'T',
+  'Q',
+  'Q',
+  'S',
+  'S',
+];
+
+const summaryDates = generateRangeDatesFromYearStart();
+
+console.log(summaryDates);
+
+
+export function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-frow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div 
+              key={`${weekDay}-${i}`} 
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          )
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map(date => {
+          return <HabitDay key={date.toString()} />
+        })}
+      </div>
+    </div>
+  )
+}
