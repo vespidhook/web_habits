ad4650cc71e44d6375f06f7b283ef86a1ba27335
Bruno Alves <vespidhook@gmail.com>
2023-07-04 22:08:49 -0300

    summary

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -13,8 +13,8 @@ const weekDays = [
 
 const summaryDates = generateRangeDatesFromYearStart();
 
-console.log(summaryDates);
-
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -35,6 +35,15 @@ export function SummaryTable() {
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
           return <HabitDay key={date.toString()} />
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+          return (
+            <div 
+              key={i} 
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          )
         })}
       </div>
     </div>
