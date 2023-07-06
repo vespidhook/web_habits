5c74b4a34829cb1270359d8c91db06a702f24fd5
Bruno Alves <vespidhook@gmail.com>
2023-07-05 22:15:49 -0300

    progressbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Plus, X } from "phosphor-react";
 import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImage from '../assets/logo.svg';
+import { NewHabitForm } from "./NewHabitForm";
 
 export function Header() {
 
@@ -26,9 +27,11 @@ export function Header() {
             <X size={24} aria-label="Fechar" />
           </Dialog.Close>
           
-          <Dialog.Title>
+          <Dialog.Title className="text-3xl leading-tight font-extrabold">
             Criar hábito
           </Dialog.Title>
+
+          <NewHabitForm />
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
