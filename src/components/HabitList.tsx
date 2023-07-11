9fe8179665eab780a4799d8c0238ed3a261b8be2
Bruno Alves <vespidhook@gmail.com>
2023-07-10 23:49:50 -0300

    list habits day

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -28,34 +28,37 @@ export function HabitList({ date }: HabitListProps) {
     }).then(response => {
       setHabitsInfo(response.data);            
     })
-  });
+  }, []);
+
+  async function handleToggleHabit(habitId: string) {    
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
+
+    await api.patch(`/habits/${habitId}/toggle`);
+
+    let completedHabits: string[] = [];
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);      
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId]
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    })
+  }
 
   const isDateInPast = dayjs(date)
     .endOf('day')
     .isBefore(new Date());
 
-    async function handleToggleHabit(habitId: string) {
-      await api.patch(`/habits/${habitId}/toggle`) 
-      
-      const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
-
-      let completedHabits: string[] = [];
-
-      if (isHabitAlreadyCompleted) {
-        completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
-
-        setHabitsInfo({
-          possibleHabits: habitsInfo!.possibleHabits,
-          completedHabits
-        })
-      } else {
-        completed
-      }
-    }
+    console.log(date);
+    
 
   return (
     <div className='mt-6 flex flex-col gap-3'> 
-      {habitsInfo?.possibleHabits.map(habit => {
+      {habitsInfo?.possibleHabits.map(habit=> {
         return (
           <Checkbox.Root
             key={habit.id}
