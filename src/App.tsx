570cc73a3e6f6515759e321c69096d2d327c06ef
Bruno Alves <vespidhook@gmail.com>
2023-07-13 00:40:47 -0300

    centralizando o summary

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './lib/dayjs';
 export function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+      <div className='px-6 flex flex-col gap-16'>
         <Header />
         <SummaryTable />
       </div>            
